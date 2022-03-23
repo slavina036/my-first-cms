9b769042e5a614fb187ac344be79ff33243e255c
Vera Slavina <sv@mitlabs.ru>
2022-03-23 12:09:57 +0300

    test

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ try {
     // Настройки БД и остальных параметров будем хранить в массиве
     $CmsConfiguration = array(); 
     $CmsConfiguration["DB_DSN"] = "mysql:host=localhost;dbname=cms;charset=utf8;" ;
-    $CmsConfiguration["DB_USERNAME"] = "root";
+    $CmsConfiguration["DB_USERNAME"] = "myuser";
     $CmsConfiguration["DB_PASSWORD"] = "qwe123";
     
     // Объявление констант, используемых в проекте
