b1af088c6b54fb220020baedcfd8cf41d0cd6c09
Vera Slavina <sv@mitlabs.ru>
2022-03-23 12:27:24 +0300

    Практическое задание 1

--- templates/homepage.php
+++ templates/homepage.php
@@ -26,7 +26,7 @@
                     </span>
                 <?php } ?>
             </h2>
-            <p class="summary"><?php echo htmlspecialchars($article->summary)?></p>
+            <p class="summary"><?php echo mb_substr(htmlspecialchars($article->content), 0, 50, 'utf8').'...'?></p>
             <img id="loader-identity" src="JS/ajax-loader.gif" alt="gif">
             
             <ul class="ajax-load">
